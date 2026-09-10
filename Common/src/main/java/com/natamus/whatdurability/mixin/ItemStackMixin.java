9e9808226fa7d578710ab3ec41d64547548b2a2a
Janucat <jan.rovatti@gmail.com>
2026-09-10 18:18:54 +0200

    .

--- Common/src/main/java/com/natamus/whatdurability/mixin/ItemStackMixin.java
+++ Common/src/main/java/com/natamus/whatdurability/mixin/ItemStackMixin.java
@@ -11,17 +11,38 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class ItemStackMixin {
 	@Inject(method = "isDamaged()Z", at = @At(value = "HEAD"), cancellable = true)
 	public void isDamaged(CallbackInfoReturnable<Boolean> cir) {
+		ItemStack itemStack = (ItemStack)(Object)this;
+
+		if (isFilledCanteen(itemStack)) {
+			return;
+		}
+
 		cir.setReturnValue(false);
 	}
 
 	@Inject(method = "getDamageValue()I", at = @At(value = "HEAD"), cancellable = true)
 	public void getDamageValue(CallbackInfoReturnable<Integer> cir) {
+		ItemStack itemStack = (ItemStack)(Object)this;
+
+		if (isFilledCanteen(itemStack)) {
+			return;
+		}
+
 		cir.setReturnValue(0);
 	}
 
 	@Inject(method = "setDamageValue(I)V", at = @At(value = "HEAD"))
 	public void setDamageValue(int damage, CallbackInfo ci) {
 		ItemStack itemStack = (ItemStack)(Object)this;
+
+		if (isFilledCanteen(itemStack)) {
+			return;
+		}
+		
 		itemStack.getOrCreateTag().putInt("Damage", 0);
+	}
+
+	private boolean isFilledCanteen(ItemStack itemStack) {
+		return itemStack.getItem().getClass().getSimpleName().equals("FilledCanteenItem");
 	}
 }
