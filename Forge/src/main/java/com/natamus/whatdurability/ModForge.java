4ce65ccb3e7a020e4bd0789363e22c86081bf1a4
Janucat <jan.rovatti@gmail.com>
2026-09-10 18:56:49 +0200

    modididi

--- Forge/src/main/java/com/natamus/whatdurability/ModForge.java
+++ Forge/src/main/java/com/natamus/whatdurability/ModForge.java
@@ -1,7 +1,5 @@
 package com.natamus.whatdurability;
 
-import com.natamus.collective.check.RegisterMod;
-import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.whatdurability.util.Reference;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
@@ -10,26 +8,18 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(Reference.MOD_ID)
 public class ModForge {
-	
-	public ModForge() {
-		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
-			return;
-		}
 
+	public ModForge() {
 		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
 		ModCommon.init();
-
-		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
 	}
 
 	private void loadComplete(final FMLLoadCompleteEvent event) {
-
 	}
 
 	private static void setGlobalConstants() {
-
 	}
 }
